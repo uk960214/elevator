0321fa0059b5486f02f35a0941203d1ee8b3cc4a
koowoy <uk960214@gmail.com>
2021-08-23 23:32:51 +0900

    complete router, working on transition

--- src/App.js
+++ src/App.js
@@ -1,66 +1,59 @@
-import React, { useState, useEffect } from "react";
-import useSound from "use-sound";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  NavLink,
+} from "react-router-dom";
+import { CSSTransition } from "react-transition-group";
 
 import "./App.css";
-import Button from "./components/Button";
-import DoorContent from "./components/DoorContent";
-
-import openSfx from "./sound/open.mp3";
-import closeSfx from "./sound/close.mp3";
-import moveSfx from "./sound/move.mp3";
-import arriveSfx from "./sound/arrive.wav";
-
-import bgm from "./sound/bgm.mp3";
+import Door from "./containers/Door";
+import Bulletin from "./containers/Bulletin";
 
 function App() {
-  const [floor, setFloor] = useState(1);
-  const [isReady, setReady] = useState(false);
-
-  const [openPlay] = useSound(openSfx, { playbackRate: 0.6 });
-  const [closePlay] = useSound(closeSfx, { playbackRate: 0.6 });
-  const [movePlay] = useSound(moveSfx, { volume: 0.3 });
-  const [arrivePlay] = useSound(arriveSfx);
-  const [bgmPlay] = useSound(bgm, { volume: 0.3, interrupt: true });
-
-  const changeFloor = (floorNum) => {
-    if (floor !== floorNum) {
-      setReady(false);
-      closePlay();
-      setTimeout(() => movePlay(), 4500);
-      setTimeout(() => setFloor(floorNum), 4000);
-      setTimeout(() => arrivePlay(), 9000);
-      setTimeout(() => {
-        openPlay();
-        setReady(true);
-      }, 9000);
-    }
-  };
-
-  const doorOpen = () => {
-    openPlay();
-    setReady(true);
-  };
-
-  const playBgm = () => {
-    bgmPlay();
-  };
-
-  useEffect(() => {
-    setTimeout(() => doorOpen(), 2000);
-    return;
-  }, []);
-
-  const Buttons = [];
-  for (let i = 1; i < 4; i++) {
-    Buttons.push(<Button key={i} floorNum={i} changeFloor={changeFloor} />);
-  }
+  const routes = [
+    { path: "/", name: "Door", Component: Door },
+    { path: "/bulletin", name: "Bulletin", Component: Bulletin },
+  ];
 
   return (
-    <div className="App">
-      <DoorContent className="content" floor={floor} isReady={isReady} />
-      {Buttons}
-      <input type="button" value="play BGM" onClick={() => playBgm()} />
-    </div>
+    <Router>
+      <div>
+        <ul>
+          {routes.map((route) => (
+            <li>
+              <Link
+                key={route.path}
+                as={NavLink}
+                to={route.path}
+                activeClassName="active"
+                exact
+              >
+                {route.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="container">
+        {routes.map(({ path, Component }) => (
+          <Route key={path} exact path={path}>
+            {({ match }) => (
+              <CSSTransition
+                in={match != null}
+                classNames="slide"
+                timeout={{ enter: 5000, exit: 5000 }}
+              >
+                <div className="slide">
+                  <Component />
+                </div>
+              </CSSTransition>
+            )}
+          </Route>
+        ))}
+      </div>
+    </Router>
   );
 }
 
---
+++ src/containers/Bulletin.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import useSound from "use-sound";
+
+import Button from "../components/Button";
+import DoorContent from "../components/DoorContent";
+
+import openSfx from "../sound/open.mp3";
+import closeSfx from "../sound/close.mp3";
+import moveSfx from "../sound/move.mp3";
+import arriveSfx from "../sound/arrive.wav";
+
+import bgm from "../sound/bgm.mp3";
+
+export default function Bulletin() {
+  const [floor, setFloor] = useState(1);
+  const [isReady, setReady] = useState(false);
+
+  const [openPlay] = useSound(openSfx, { playbackRate: 0.6 });
+  const [closePlay] = useSound(closeSfx, { playbackRate: 0.6 });
+  const [movePlay] = useSound(moveSfx, { volume: 0.3 });
+  const [arrivePlay] = useSound(arriveSfx);
+
+  const changeFloor = (floorNum) => {
+    if (floor !== floorNum) {
+      setReady(false);
+      closePlay();
+      setTimeout(() => movePlay(), 4500);
+      setTimeout(() => setFloor(floorNum), 4000);
+      setTimeout(() => arrivePlay(), 9000);
+      setTimeout(() => {
+        openPlay();
+        setReady(true);
+      }, 9000);
+    }
+  };
+
+  const doorOpen = () => {
+    openPlay();
+    setReady(true);
+  };
+
+  const [bgmPlay] = useSound(bgm, { volume: 0.3, interrupt: true });
+
+  const playBgm = () => {
+    bgmPlay();
+  };
+
+  useEffect(() => {
+    setTimeout(() => doorOpen(), 2000);
+    return;
+  }, []);
+
+  const Buttons = [];
+  for (let i = 1; i < 4; i++) {
+    Buttons.push(<Button key={i} floorNum={i} changeFloor={changeFloor} />);
+  }
+
+  return (
+    <div className="Door">
+      <DoorContent className="content" floor={floor} isReady={isReady} />
+      {Buttons}
+      <input type="button" value="play BGM" onClick={() => playBgm()} />
+    </div>
+  );
+}
